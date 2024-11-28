36ce49298ca3e804cff60add3301f0b8c5ebfb32
Honeylii <oriadeoxy@gmail.com>
2024-11-28 10:56:48 +0100

    Feat: About Page

---
+++ constants/about.ts
@@ -0,0 +1,15 @@
+import aboutStaffImg from "@/public/images/help_station_about_staffs.svg";
+import aboutConferenceImg from "@/public/images/help_station_about_conference.svg";
+import aboutBusesImg from "@/public/images/help_station_about_buses.svg";
+import aboutAwarenessImg from "@/public/images/help_station_about_womerawareness.svg";
+import aboutWorkesImg from "@/public/images/help_station_about_workes.svg";
+import aboutEventsImg from "@/public/images/help_station_about_events.svg";
+
+export const aboutImages = [
+  aboutStaffImg,
+  aboutConferenceImg,
+  aboutBusesImg,
+  aboutAwarenessImg,
+  aboutWorkesImg,
+  aboutEventsImg,
+];
