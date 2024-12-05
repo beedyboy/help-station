f9eb826d83b793fbe9326049ae1e1ef3a2544aa9
Honeylii <oriadeoxy@gmail.com>
2024-12-05 13:49:12 +0100

    chore: Adding FAQ page

---
+++ app/faq/page.tsx
@@ -0,0 +1,11 @@
+import FAQ from "@/domain/faq";
+
+function FAQPage() {
+  return (
+    <div className="w-full">
+      <FAQ />
+    </div>
+  );
+}
+
+export default FAQPage;
---
+++ constants/faq.ts
@@ -0,0 +1,115 @@
+type IFAQ = {
+  id: number;
+  question: string;
+  answer: string;
+  isOpen: boolean;
+};
+
+export const faqData: IFAQ[] = [
+  {
+    id: 0,
+    question: "What is HelpStation?",
+    answer:
+      "HelpStation is a health tech company dedicated to leveraging innovative technology to provide prompt access to emergency services in Nigeria. ",
+    isOpen: false,
+  },
+  {
+    id: 1,
+    question: "What services does HelpStation provide?",
+    answer:
+      "HelpStation offers services like 24/7 Help Center support, instant ambulance request, ambulance on standby, first aid boxes & Emergency Preparedness Trainings and more.",
+    isOpen: false,
+  },
+  {
+    id: 2,
+    question: "How does HelpStation ensure quick response times?",
+    answer:
+      "HelpStation utilises a proprietary technology platform to connect first responders, ambulances, and hospitals, ensuring rapid and coordinated emergency responses.",
+    isOpen: false,
+  },
+  {
+    id: 3,
+    question: " Who can use HelpStation's services?",
+    answer:
+      "  HelpStation's services are available to individuals, corporates, and government entities.",
+    isOpen: false,
+  },
+  {
+    id: 4,
+    question: "How do I contact HelpStation in an emergency?",
+    answer:
+      "Call HelpStation’s number 0913-200-0013, available 24/7, to reach a qualified medical professional and get immediate assistance.",
+    isOpen: false,
+  },
+  {
+    id: 5,
+    question: "Can I use HelpStation's services if I have health insurance?",
+    answer:
+      "No. At the moment, we do not accept health insurance as a form of payment",
+    isOpen: false,
+  },
+  {
+    id: 6,
+    question:
+      " What makes HelpStation different from other ambulance services?",
+    answer:
+      "HelpStation combines on-site first responder services with medical transportation, providing comprehensive pre-hospital care.",
+    isOpen: false,
+  },
+  {
+    id: 7,
+    question: "Is HelpStation a non-profit organization?",
+    answer:
+      "No, HelpStation is a privately owned company that charges a subscription fee to provide affordable, high-quality emergency services.",
+    isOpen: false,
+  },
+  {
+    id: 8,
+    question: " What areas does HelpStation cover?",
+    answer:
+      "HelpStation We have a network of over 200+ ambulance partners and are currently operating in three (3) states, with plans of rapid expansion in Nigeria and across Africa.",
+    isOpen: false,
+  },
+  {
+    id: 9,
+    question: "How do I become a HelpStation partner?",
+    answer:
+      "Visit the “Become a Partner” section on the HelpStation website to learn about partnership opportunities and apply.",
+    isOpen: false,
+  },
+  {
+    id: 10,
+    question: "What training do HelpStation first responders receive?",
+    answer:
+      "HelpStation first responders are highly trained and certified to handle a wide range of medical emergencies.",
+    isOpen: false,
+  },
+  {
+    id: 12,
+    question: "Can I get a subscription for HelpStation services?",
+    answer:
+      "Yes, HelpStation offers subscription plans that allow individuals and organisations to access their services whenever needed",
+    isOpen: false,
+  },
+  {
+    id: 13,
+    question: " What should I do if I need an ambulance?",
+    answer:
+      "Call HelpStation’s emergency number to dispatch an ambulance to your location promptly",
+    isOpen: false,
+  },
+  {
+    id: 14,
+    question: " Does HelpStation offer first aid and CPR training?",
+    answer:
+      "Yes, HelpStation provides comprehensive first aid and CPR training to individuals and organisations both in person and online.",
+    isOpen: false,
+  },
+  {
+    id: 15,
+    question: "How can I stay updated with HelpStation news and updates?",
+    answer:
+      "Subscribe to the HelpStation newsletter on their website to receive news and updates about their services and impact. Also follow our social media pages to get first hand information about our new services.",
+    isOpen: false,
+  },
+];
