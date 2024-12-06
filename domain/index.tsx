b541e17fd65ed9cfc1819648e7be79faee9843f3
Bolade Akinniyi <bolade@Bolades-MacBook-Pro.local>
2024-12-06 06:25:34 +0100

    chore: fix contact and about

--- domain/index.tsx
+++ domain/index.tsx
@@ -77,9 +77,9 @@ const HomePage: React.FC = () => {
           </div>
         </div>
 
-        <div className="w-[100%] flex justify-center items-center bg-[#F9F9FC]">
+        <div className="w-full flex justify-center items-center bg-[#F9F9FC]">
           <div className="w-[90%] flex justify-center items-center px-4 py-8 md:px-0 md:py-0">
-            <BrandTestimony />
+            {/* <BrandTestimony /> */}
           </div>
         </div>
 
