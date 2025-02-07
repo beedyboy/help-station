e44b1088cfb1e8cf5d859c0eadb389fd089a6828
Honeylii <oriadeoxy@gmail.com>
2025-02-07 23:33:03 +0100

    fix: Romoving framer-motion package

--- components/countrySelector/country.tsx
+++ components/countrySelector/country.tsx
@@ -1,8 +1,5 @@
 import { COUNTRIES } from "@/constants/countries";
 import { SelectMenuOption } from "@/constants/types";
-
-import { AnimatePresence } from "framer-motion";
-import { motion } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
@@ -79,76 +76,71 @@ export default function CountrySelector({
           </span>
         </button>
 
-        <AnimatePresence>
-          {open && (
-            <motion.ul
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.1 }}
-              className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-80 rounded-md text-base ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-              tabIndex={-1}
-              role="listbox"
-              aria-labelledby="listbox-label"
-              aria-activedescendant="listbox-option-3"
-            >
-              <div className="max-h-64 scrollbar scrollbar-track-gray-100 scrollbar-thumb-gray-300 hover:scrollbar-thumb-gray-600 scrollbar-thumb-rounded scrollbar-thin overflow-y-scroll">
-                {COUNTRIES.filter((country) =>
+        {open && (
+          <ul
+            className={`absolute z-10 mt-1 w-full bg-white shadow-lg max-h-80 rounded-md text-base ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm 
+      transition-opacity duration-100 ease-in-out ${
+        open ? "opacity-100 scale-100" : "opacity-0 scale-95"
+      }`}
+            tabIndex={-1}
+            role="listbox"
+            aria-labelledby="listbox-label"
+            aria-activedescendant="listbox-option-3"
+          >
+            <div className="max-h-64 scrollbar scrollbar-track-gray-100 scrollbar-thumb-gray-300 hover:scrollbar-thumb-gray-600 scrollbar-thumb-rounded scrollbar-thin overflow-y-scroll">
+              {COUNTRIES.filter((country) =>
+                country.title.toLowerCase().startsWith(query.toLowerCase())
+              ).length === 0 ? (
+                <li className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9">
+                  Select your country
+                </li>
+              ) : (
+                COUNTRIES.filter((country) =>
                   country.title.toLowerCase().startsWith(query.toLowerCase())
-                ).length === 0 ? (
-                  <li className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9">
-                    Select your country
-                  </li>
-                ) : (
-                  COUNTRIES.filter((country) =>
-                    country.title.toLowerCase().startsWith(query.toLowerCase())
-                  ).map((value, index) => (
-                    <li
-                      key={`${id}-${index}`}
-                      className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9 flex items-center hover:bg-gray-50 transition"
-                      id="listbox-option-0"
-                      role="option"
-                      aria-selected={value.value === selectedValue.value}
-                      onClick={() => {
-                        onChange(value.value);
-                        setQuery("");
-                        onToggle();
-                      }}
-                    >
-                      <Image
-                        alt={`${value.value}`}
-                        src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${value.value}.svg`}
-                        className={"inline mr-2 h-4 rounded-sm"}
-                        width={20}
-                        height={20}
-                      />
-                      <span className="font-normal truncate">
-                        {value.title}
+                ).map((value, index) => (
+                  <li
+                    key={`${id}-${index}`}
+                    className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9 flex items-center hover:bg-gray-50 transition"
+                    id="listbox-option-0"
+                    role="option"
+                    aria-selected={value.value === selectedValue.value}
+                    onClick={() => {
+                      onChange(value.value);
+                      setQuery("");
+                      onToggle();
+                    }}
+                  >
+                    <Image
+                      alt={`${value.value}`}
+                      src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${value.value}.svg`}
+                      className={"inline mr-2 h-4 rounded-sm"}
+                      width={20}
+                      height={20}
+                    />
+                    <span className="font-normal truncate">{value.title}</span>
+                    {value.value === selectedValue.value && (
+                      <span className="text-blue-600 absolute inset-y-0 right-0 flex items-center pr-8">
+                        <svg
+                          className="h-5 w-5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
                       </span>
-                      {value.value === selectedValue.value && (
-                        <span className="text-blue-600 absolute inset-y-0 right-0 flex items-center pr-8">
-                          <svg
-                            className="h-5 w-5"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </span>
-                      )}
-                    </li>
-                  ))
-                )}
-              </div>
-            </motion.ul>
-          )}
-        </AnimatePresence>
+                    )}
+                  </li>
+                ))
+              )}
+            </div>
+          </ul>
+        )}
       </div>
     </div>
   );
